d1610b215b9bc578219f81886581f984410b23f8
Syeda Mahnoor <syedamahnoor9e@gmail.com>
2026-05-03 18:28:11 +0500

    write flask backend logic

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import uuid
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/create")
+
+@app.route("/create", methods=["GET", "POST"])
 def create():
-    return render_template("create.html")
+    myid = uuid.uuid4()
+    if request.method == "POST":
+        print(request.files.keys())
+        for key, value in request.files.items():
+            print(key, value)
+    return render_template("create.html", myid=myid)
+
 
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
+
 
 app.run(debug=True)
