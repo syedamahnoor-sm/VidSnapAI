757d2e15003b6554db097b7c257a24f21193beb1
Syeda Mahnoor <syedamahnoor9e@gmail.com>
2026-05-03 20:00:51 +0500

    upload files to server

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
 import uuid
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = "user_uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -14,8 +20,25 @@ def create():
     myid = uuid.uuid4()
     if request.method == "POST":
         print(request.files.keys())
+        rec_id = request.form.get("uuid")
+        desc = request.form.get("text")
         for key, value in request.files.items():
             print(key, value)
+            # Upload the file
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if not (
+                    os.path.exists(os.path.join(app.config["UPLOAD_FOLDER"], rec_id))
+                ):
+                    os.mkdir(os.path.join(app.config["UPLOAD_FOLDER"], rec_id))
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], rec_id, filename))
+
+            # Capture the description and save it to a file
+            with open(
+                os.path.join(app.config["UPLOAD_FOLDER"], rec_id, "desc.txt"), "w"
+            ) as f:
+                f.write(desc)
     return render_template("create.html", myid=myid)
 
 
